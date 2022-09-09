ab948b281dd8ed6deecb640ffbe10fd34ce76749
Roger S. Grading <roger@grading.no>
2022-09-09 09:43:24 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿namespace Azure_QueueStorage_Fiddle;
+
+using System.Threading.Tasks;
 using Azure.Storage.Queues;
 using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        
+    }
+}
